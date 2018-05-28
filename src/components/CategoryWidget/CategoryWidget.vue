cdea0bdc3609b15875e2b23f0093b5ee96e84e28
rubenmoya <ruben@carto.com>
2018-05-28 12:15:33 +0200

    Render categories ✨

--- src/components/CategoryWidget/CategoryWidget.vue
+++ src/components/CategoryWidget/CategoryWidget.vue
@@ -1,13 +1,50 @@
 <template>
-  <button v-on:click="handleClick">{{ msg }}</button>
+  <section>
+    <header>
+      <h1 class="CDB-Title">{{title}}</h1>
+      <p>{{description}}</p>
+    </header>
+
+    <ul>
+      <li v-for="category in categories" v-on:click="onCategoryClick(category.name)">
+        <p>{{category.name}}</p>
+        <span>{{category.value}}</span>
+        <div>{{category.percentage}}</div>
+        <div>Selected: {{category.selected ? 'YES' : 'NO'}}</div>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
   export default {
-    props: ['msg'],
+    props: ['title', 'description', 'data', 'max'],
+    data: function () {
+      return {
+        selected: [],
+      };
+    },
+    computed: {
+      categories: function () {
+        const categories = JSON.parse(this.data);
+        return categories.map(category => ({
+          ...category,
+          percentage: category.value / this.max * 100,
+          selected: this.selected.includes(category.name)
+        }))
+      },
+    },
     methods: {
-      handleClick: function (event) {
-        this.$emit('onCategoryClicked', { category: 'test' })
+      onCategoryClick: function (categoryName) {
+        this._toggleSelected(categoryName);
+        this.$emit('onCategoryClicked', this.selected);
+      },
+      _toggleSelected: function (categoryName) {
+        const nextSelected = this.selected.includes(categoryName)
+          ? this.selected.filter(category => category !== categoryName)
+          : [...this.selected, categoryName];
+
+        this.selected = nextSelected;
       }
     }
   }
